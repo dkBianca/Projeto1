59dfab13e4bcbaece67d38d4beacf2e65a84eaa7
dkBianca <141647072+dkBianca@users.noreply.github.com>
2023-09-25 11:31:57 -0400

    Update radio.js

--- radio.js
+++ radio.js
@@ -1,14 +1,14 @@
 let musicas = [
-    {titulo:'Die For You', artista:'Joji', src:'mscRadio/SnapInsta.io - Joji - Die For You (128 kbps).mp3', img:'imgRadio/iradio.jpg'},
+    {titulo:'Die For You', artista:'Joji', src:'mscRadio/Joji - Die For You.mp3', img:'imgRadio/iradio.jpg'},
     {titulo:'So I Danced', artista:'DPR IAN', src:'mscRadio/DPR IAN - So I Danced.mp3', img:'imgRadio/iradio.jpg'},
     {titulo:'fOol fOr YoU', artista:'Zayn', src:'mscRadio/zayn- fool for you.mp3', img:'imgRadio/iradio.jpg'},
     {titulo:'Ballroom Extravaganza', artista:'DPR IAN', src:'mscRadio/DPR IAN - Ballroom Extravaganza.mp3', img:'imgRadio/iradio.jpg'},
-    {titulo:'Nerves', artista:'DPR IAN', src:'mscRadio/SnapInsta.io-DPR IAN - Nerves (OFFICIAL M_V).mp3', img:'imgRadio/iradio.jpg'},
+    {titulo:'Nerves', artista:'DPR IAN', src:'mscRadio/DPR IAN - Nerves.mp4', img:'imgRadio/iradio.jpg'},
     {titulo:'Painkiller', artista:'RUEL', src:'mscRadio/SnapInsta.io - Ruel - Painkiller (Official Video) (128 kbps).mp3', img:'imgRadio/iradio.jpg'},
-    {titulo:'007', artista:'Tabber', src:'mscRadio/SnapInsta.io-007 (feat. Syd)(720p).mp3', img:'imgRadio/iradio.jpg'},
-    {titulo:'Gimme Love', artista:'Joji', src:'mscRadio/SnapInsta.io-Joji - Gimme Love (Official Video).mp3', img:'imgRadio/iradio.jpg'},
-    {titulo:'SLOW DANCING IN THE DARK', artista:'Joji', src:'mscRadio/SnapInsta.io-Joji - SLOW DANCING IN THE DARK.mp3', img:'imgRadio/iradio.jpg'},
-    {titulo:'Back To Me', artista:'The Rose', src:'mscRadio/SnapInsta.io-The Rose (더로즈) – Back To Me _ Official Video.mp3', img:'imgRadio/iradio.jpg'}
+    {titulo:'007', artista:'Tabber', src:'mscRadio/007 (feat. Syd)(720p).mp4', img:'imgRadio/iradio.jpg'},
+    {titulo:'Gimme Love', artista:'Joji', src:'mscRadio/Joji - Gimme Love.mp4', img:'imgRadio/iradio.jpg'},
+    {titulo:'SLOW DANCING IN THE DARK', artista:'Joji', src:'mscRadio/Joji - SLOW DANCING IN THE DARK.mp4', img:'imgRadio/iradio.jpg'},
+    {titulo:'Back To Me', artista:'The Rose', src:'mscRadio/The Rose (더로즈) – Back To Me.mp4', img:'imgRadio/iradio.jpg'}
 ];
 
 
